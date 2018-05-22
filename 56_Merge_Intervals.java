0795ab03de6e8760d90489bbc4d2b41bbaf391c3
Mol1122 <xchen141@terpmail.umd.edu>
2018-05-22 00:27:27 -0400

    5/21

---
+++ 56_Merge_Intervals.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        List<Interval> ans = new ArrayList<Interval>();
+        intervals.sort(Comparator.comparing(i -> i.start)); //i -> i.start is lambda expression
+        Interval last = null;
+        for (Interval item : intervals) {
+            if (last == null || last.end < item.start) {
+                ans.add(item);
+                last = item;
+            } else {
+                last.end = Math.max(last.end, item.end);
+            }
+        }
+        return ans;
+    }
+}
+
+//语法难点：如何sort list of intervals based on start
