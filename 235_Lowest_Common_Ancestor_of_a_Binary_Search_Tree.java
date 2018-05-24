6bfce0ad329355df4e94e4a9b8023d12b55d4196
Mol1122 <xchen141@terpmail.umd.edu>
2018-05-24 03:19:17 -0400

    5/23

---
+++ 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+             return null;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if ( p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
+
+/* 很简单的思路就是看两个值在root的哪边：
+
+两个值都在左边，则LCA在左边
+两个值都在右边，则LCA在右边
+一个在左一个在右，则说明LCA就是当前的root节点。 */
