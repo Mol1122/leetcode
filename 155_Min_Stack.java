299d3cc2128d93dbef74c41a1d197a6bb32e04e4
Mol1122 <xchen141@terpmail.umd.edu>
2018-05-28 01:29:45 -0700

    5/27

---
+++ 155_Min_Stack.java
@@ -0,0 +1,43 @@
+class MinStack {
+    Stack<Integer> stack; 
+    Stack<Integer> minStack; 
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty()) {
+            minStack.push(x);
+        } else {
+            minStack.push(Math.min(x, minStack.peek()));
+        }
+    }
+    
+    public void pop() {
+        minStack.pop();
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+/* 算法：利用minStack去保存最小值 */
