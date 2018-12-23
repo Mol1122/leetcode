c19dd5c5fff213958167f2ed13527568a4c3cac4
Mol Chen <molchen@MacBook-Pro-85.local>
2018-12-23 11:59:19 -0500

    12/23

---
+++ 126. Max Tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param A: Given an integer array with no duplicates.
+     * @return: The root of max tree.
+     */
+    public TreeNode maxTree(int[] A) {
+        if (A == null || A.length == 0) {
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        for (int i = 0; i <= A.length; i++) {
+            TreeNode right = i == A.length ? new TreeNode(Integer.MAX_VALUE) :
+                            new TreeNode(A[i]);
+            while (!stack.isEmpty()) {
+                if (right.val > stack.peek().val) {
+                    TreeNode nowNode = stack.pop();
+                    if (stack.isEmpty()) {
+                        right.left = nowNode;
+                    } else {
+                        TreeNode left = stack.peek();
+                        if (left.val > right.val) {
+                            right.left = nowNode;
+                        } else {
+                            left.right = nowNode;
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+            stack.push(right);
+        }
+        return stack.peek().left;
+    }
+}
+
+/* 单调递减栈 */
--- 85. Maximal Rectangle.java
+++ 85. Maximal Rectangle.java
@@ -5,43 +5,41 @@ class Solution {
         }
         int n = matrix.length;
         int m = matrix[0].length;
-        int[][] heights = new int[n][m + 1];
+        int[][] heights = new int[n][m];
         
+        //以这一行为bottom,往上数有几个1
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == '0') {
-                    heights[i][j] = 0;
-                } else {
-                    heights[i][j] = i == 0 ? 1 : heights[i - 1][j] + 1;
+                if (matrix[i][j] == '1') {
+                    if (i == 0) {
+                        heights[i][j] = 1;
+                    } else {
+                        heights[i][j] = heights[i - 1][j] + 1;
+                    }
                 }
             }
         }
-        int maxArea = 0;
+        int max = 0;
         for (int i = 0; i < n; i++) {
-            int temp = findMaxInHist(heights[i]);
-            maxArea = Math.max(maxArea, temp);
+            int area = getArea(heights[i]);
+            max = Math.max(max, area);
         }
-        return maxArea;
+        return max;
     }
     
-    private int findMaxInHist(int[] height) {
+    private int getArea(int[] height) {
         Stack<Integer> stack = new Stack<>();
-        int i = 0;
         int max = 0;
         
-        while (i < height.length) {
-            if (stack.isEmpty() || height[i] >= height[stack.peek()]) { //ensuring increasing
-                stack.push(i++); //stack里面放着的还没有i++
-            } else {
-                int t = stack.pop();
-                max = Math.max(max, height[t] * (stack.isEmpty() ? i : (i - stack.peek() - 1)));
+        for (int i = 0; i <= height.length; i++) {
+            int curr = i == height.length ? -1 : height[i];
+            while (!stack.isEmpty() && curr < height[stack.peek()]) {
+                int h = height[stack.pop()];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, h * w);
             }
+            stack.push(i);
         }
-        
         return max;
     }
 }
-
-/* 算法：序列型动态规划，但是本题用了stack去处理
-** 时间复杂度：O(nm) 
-** 难点： int[][] heights = new int[n][m + 1]; “m+1”因为stack要push之后总要前进一个位置，为了防止溢出 */
