c2c9d84d96f032accb4982d52a036a4e4b2cd1c6
Xi Chen <mol.chen@live.com>
2019-03-22 14:21:03 -0400

    save untrack

--- 110. Minimum Path Sum.java
+++ 110. Minimum Path Sum.java
@@ -1,31 +1,31 @@
-class Solution {
+public class Solution {
+    /**
+     * @param grid: a list of lists of integers
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
     public int minPathSum(int[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
         int n = grid.length;
         int m = grid[0].length;
-        int[][] f = new int[n][m];
+        int[][] f = new int[2][m];
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                f[i][j] = Integer.MAX_VALUE;
+                f[i%2][j] = Integer.MAX_VALUE;
                 if (i == 0 && j == 0) {
-                    f[i][j] = grid[0][0];
+                    f[i%2][j] = grid[0][0];
                     continue;
                 }
                 if (i > 0) {
-                    f[i][j] = Math.min(f[i][j], f[i - 1][j] + grid[i][j]);
+                    f[i%2][j] = Math.min(f[i%2][j], f[(i - 1)%2][j] + grid[i][j]);
                 }
                 if (j > 0) {
-                    f[i][j] = Math.min(f[i][j], f[i][j - 1] + grid[i][j]);
+                    f[i%2][j] = Math.min(f[i%2][j], f[i%2][j - 1] + grid[i][j]);
                 }
             }
         }
-        return f[n - 1][m - 1];
+        return f[(n - 1)%2][m - 1];
     }
 }
-
-/* 算法：坐标型dp 
-** 时间复杂度：O(nm)
-** 空间复杂度：O(nm) */
