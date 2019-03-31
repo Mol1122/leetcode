637a79f3b34886e878f01fb03ecdda9554484226
Xi Chen <mol.chen@live.com>
2019-03-31 01:15:46 -0400

    4/30

--- 407. Trapping Rain Water II.java
+++ 407. Trapping Rain Water II.java
@@ -1,52 +1,61 @@
 class Solution {
-    public int trapRainWater(int[][] heightMap) {
-        if (heightMap == null || heightMap.length == 0) {
+    public int trapRainWater(int[][] heights) {
+        if (heights == null || heights.length == 0) {
             return 0;
         }
-        int n = heightMap.length;
-        int m = heightMap[0].length;
-        PriorityQueue<Cell> pq = new PriorityQueue<>((a, b) -> a.h - b.h);
-        int[][] visited = new int[n][m];
+        Queue<Pair> minheap = new PriorityQueue<>(new Comparator<Pair>() {
+            public int compare(Pair a, Pair b) {
+                return a.height - b.height;
+            }
+        });
+        int n = heights.length;
+        int m = heights[0].length;
+        boolean[][] visited = new boolean[n][m];
         
         for (int i = 0; i < n; i++) {
-            pq.offer(new Cell(i, 0, heightMap[i][0]));
-            pq.offer(new Cell(i, m - 1, heightMap[i][m - 1]));
-            visited[i][0] = 1;
-            visited[i][m - 1] = 1;
+            minheap.offer(new Pair(i, 0, heights[i][0]));
+            minheap.offer(new Pair(i, m - 1, heights[i][m - 1]));
+            visited[i][0] = true;
+            visited[i][m - 1] = true;
         }
         for (int j = 0; j < m; j++) {
-            pq.offer(new Cell(0, j, heightMap[0][j]));
-            pq.offer(new Cell(n - 1, j, heightMap[n - 1][j]));
-            visited[0][j] = 1;
-            visited[n - 1][j] = 1;
+            minheap.offer(new Pair(0, j, heights[0][j]));
+            minheap.offer(new Pair(n - 1, j, heights[n - 1][j]));
+            visited[0][j] = true;
+            visited[n - 1][j] = true;
         }
-        int result = 0;
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
-        while (!pq.isEmpty()) {
-            Cell c = pq.poll();
+        int sum = 0;
+        
+        while (!minheap.isEmpty()) {
+            Pair curr = minheap.poll();
+            
             for (int i = 0; i < 4; i++) {
-                int nx = c.x + dx[i];
-                int ny = c.y + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && visited[nx][ny] == 0) {
-                    visited[nx][ny] = 1;
-                    pq.offer(new Cell(nx, ny, Math.max(c.h, heightMap[nx][ny])));
-                    result += Math.max(0, c.h - heightMap[nx][ny]);
+                int nx = curr.x + dx[i];
+                int ny = curr.y + dy[i];
+                
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny]) {
+                    continue;
                 }
+                minheap.offer(new Pair(nx, ny, Math.max(curr.height, heights[nx][ny])));
+                sum += Math.max(0, curr.height - heights[nx][ny]);
+                visited[nx][ny] = true;
             }
         }
-        return result;
+        return sum;
     }
 }
 
-class Cell {
-    int x, y, h;
-    public Cell(int x, int y, int h) {
+class Pair {
+    int x, y, height;
+    
+    public Pair(int x, int y, int height) {
         this.x = x;
         this.y = y;
-        this.h = h;
+        this.height = height;
     }
 }
 
-/* 算法：跟一维的思路一样，从四周往里灌水，用heap去track最低点
-** 难点：33行，在更新高度的时候，那个高选哪个 */
+/* 时间复杂度：O(mn * log(m + n))
+** 空间复杂度：O(n^2) */
