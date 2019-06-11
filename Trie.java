8ec785b3f7a75fd26370eb3c81340febec13ff51
Xi Chen <mol.chen@live.com>
2019-06-11 13:35:38 -0700

    6/10

--- Trie.java
+++ Trie.java
@@ -1,5 +1,5 @@
 class TrieNode {
-	char c;
+    int count; //how many words are on this subtree
 	Map<Character, TrieNode> children;
 	boolean isWord;
 
@@ -52,6 +52,58 @@ public class Trie {
             curr = curr.children.get(c);
         }
         return true;
+    }
+
+    public boolean delete(String word) {
+        if (!search(word)) {
+            return false;
+        }
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            TrieNode next = curr.children.get(c);
+            next.count--;
+            if (next.count == 0) {
+                curr.children.remove(word.charAt(i)); //remove leaf, 到中间就知道只剩一个了
+                return true;
+            }
+            curr = curr.children.get(c);
+        }
+        curr.isWord = false; //remove 中间，中间的下面还有另外一个单词
+        return true;
+    }
+
+    private TrieNode searchNode(String prefix) {
+        TrieNode curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                return null;
+            }
+            curr = curr.children.get(c);
+        }
+        return curr;
+    }
+
+    public List<String> findAllWordsWithPrefix(String prefix) {
+        List<String> results = new ArrayList<>();
+        TrieNode matchNode = searchNode(prefix);
+        if (prefix == null || prefix.length() == 0 || matchNode == null) {
+            return results;
+        }
+        findAllWordsUnderNode(matchNode, new StringBuilder(prefix), results);
+        return results;
+    }
+
+    private void findAllWordsUnderNode(TrieNode curr, StringBuilder sb, List<String> results) {
+        if (curr.isWord) {
+            results.add(new String(sb));
+        }
+        for (char c : curr.children.keySet()) {
+            sb.append(c);
+            findAllWordsUnderNode(curr.children.get(c), sb, results);
+            sb.remove(sb.length() - 1);
+        }
     }
 }
 
